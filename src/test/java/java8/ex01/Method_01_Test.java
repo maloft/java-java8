c0d4c0ce3b6f88178f1aed856bcd14623c561a23
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-24 11:12:53 +0200

    TP Méthode non abstraite dans une interface : Exercice 1

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -19,6 +19,15 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        
+        default int sumAge() {
+        	int result = 0;
+        	for (Person p: findAll())
+        	{
+        		result += p.getAge();
+        	}
+        	return result;
+        }
     }
     // end::IDao[]
 
@@ -49,6 +58,8 @@ public class Method_01_Test {
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
         int result = 0;
+        
+        result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -60,6 +71,8 @@ public class Method_01_Test {
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
         int result = 0;
+        
+        result = daoB.sumAge();
 
         assert result == 5050;
 
