539219cb20924e40496e93b084d0084a388d4210
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-27 12:58:37 +0200

    TP DAte and Time : Exercice 7

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,10 @@ package java8.ex07;
 import org.junit.Test;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,13 +21,16 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        Date date = new Date();
+        date.setYear(117);
+        date.setMonth(1);;
+        date.setDate(12);
 
         // TODO transformer la date en Instant
-
+        Instant instant = date.toInstant();
         // TODO transformer la date en LocalDate
 
-        LocalDate result = null;
+        LocalDate result = LocalDate.ofInstant(instant, ZoneId.systemDefault());
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
